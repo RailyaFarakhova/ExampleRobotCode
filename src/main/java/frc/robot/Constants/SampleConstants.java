6cf6e42cc2ffef8564ad6b68c304c19a2bab8132
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-05-09 17:41:34 -0700

    initial commit

---
+++ src/main/java/frc/robot/Constants/SampleConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.Constants;
+
+public class SampleConstants {
+    public static final int motorID = 1;
+    public static final double maxRange = 1;
+    
+    public static final double kP = 0.5;
+}
---
+++ src/main/java/frc/robot/Subsystems/Subsystem.java
@@ -0,0 +1,54 @@
+package frc.robot.Subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SampleConstants;
+
+public class Subsystem extends SubsystemBase {
+    private TalonFX m_motor;
+
+    private DutyCycleOut m_dutyCycleOut;
+    private PositionVoltage m_positionVoltage;
+
+    private TalonFXConfiguration m_motorConfig;
+
+    public Subsystem() {
+        m_motor = new TalonFX(SampleConstants.motorID);
+        m_dutyCycleOut = new DutyCycleOut(SampleConstants.maxRange);
+        m_positionVoltage = new PositionVoltage(0);
+        m_motorConfig = new TalonFXConfiguration(); 
+        m_motorConfig.Slot0.kP = SampleConstants.kP;
+
+        m_motor.getConfigurator().apply(m_motorConfig);
+
+    }
+
+    public void setStartPosition() {
+        m_motor.setPosition(0);
+    }
+
+    /**
+     * Controls motor power from range of -1 to 1.
+     * @param speedPercentage from -1 to 1.
+     */
+    public void motorOn(double speedPercentage) {
+        m_motor.setControl(m_dutyCycleOut.withOutput(speedPercentage)); 
+    }
+
+    public void setPosition(double motorPosition) {
+        m_motor.setControl(m_positionVoltage.withPosition(motorPosition));
+    }
+
+    //Powers off motor.
+    public void motorOff() {
+        m_motor.setControl(m_dutyCycleOut.withOutput(0));
+    }
+
+    public double getMotorRotation() {
+        return m_motor.getPosition().getValueAsDouble();
+    }
+}
